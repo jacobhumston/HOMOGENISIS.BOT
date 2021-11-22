b433622f2232a84440b55cd03b225aa3ce1a6710
Jacob Humston <57332486+jacobhumston@users.noreply.github.com>
2021-11-22 18:18:15 -0500

    Add config file.

---
+++ src/Configuration.js
@@ -0,0 +1,19 @@
+/** 
+ * @module
+ * Configuration Module
+ */
+module.exports = {
+    version: "v1.0.pre-beta",
+    client: {
+        token: "",
+        activity: {
+            switchRate: 1,
+            activities: [
+                {
+                    type: "WATCHING",
+                    content: "fun events.",
+                },
+            ],
+        },
+    },
+};
