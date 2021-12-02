11072b8471dcf9ff9cbc0dc14e0dff317c83bdd8
Jacob Humston <57332486+jacobhumston@users.noreply.github.com>
2021-12-02 10:42:23 -0500

    removed extra space and commented on config

--- src/Modules/Configuration.js
+++ src/Modules/Configuration.js
@@ -6,12 +6,19 @@
  */
 module.exports = {
     version: "v1.0.pre-beta",
+    // Current version of the bot
     permissions: {
         staffRoleID: "914287882817114123",
+        // Role that is required to use staff commands
         developerUserID: "539194357706653696",
+        // Role that is required to use developer commands
     },
     client: {
         token: require("../token.json"),
+        // Client token/password
         guildID: "837866184505294849",
+        // Guild that the client belongs in
+        guildCheckOnStartUp: true,
+        // Check that the client is only in the guild it belongs in on startup
     },
 };
