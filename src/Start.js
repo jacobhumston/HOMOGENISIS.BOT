19d6419965930eb21fa22cbfe413ea25a90cce51
Jacob Humston <57332486+jacobhumston@users.noreply.github.com>
2021-12-07 13:19:28 -0500

    fixed spacing

--- src/Start.js
+++ src/Start.js
@@ -1,10 +1,10 @@
 // require needed modules
 const
-  Logs = require("./Modules/Logs.js"),
-  GuildsCheck = require("./Modules/GuildsCheck.js"),
-  Configuration = require("./Modules/Configuration.js"),
-  Discord = require('discord.js'),
-  prettyMilliseconds = require('pretty-ms');
+    Logs = require("./Modules/Logs.js"),
+    GuildsCheck = require("./Modules/GuildsCheck.js"),
+    Configuration = require("./Modules/Configuration.js"),
+    Discord = require('discord.js'),
+    prettyMilliseconds = require('pretty-ms');
 
 // log that the start file has been executed
 // also create a new date so we can log how long it took later
@@ -13,12 +13,12 @@ Logs.log("log", "Start", "Start.js is now being executed...");
 
 // create discord client
 const Client = new Discord.Client({
-  intents: new Discord.Intents(32767),
+    intents: new Discord.Intents(32767),
 });
 
 // client login
 Client.login(Configuration.client.token).catch(error => {
-  Logs.log("log", "Start", error);
+    Logs.log("log", "Start", error);
 });
 
 // log that the start file has finsihed being executed
